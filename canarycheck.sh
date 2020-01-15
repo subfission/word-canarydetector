5ffeed540e6c792244d58a5bbf4ff2a7b9243663
subfission <subfission@users.noreply.github.com>
2020-01-15 16:58:24 -0700

    Update canarycheck.sh

--- canarycheck.sh
+++ canarycheck.sh
@@ -6,9 +6,9 @@ file=$1
 
 if [ ! -f $file ]; then
   echo
-  echo "Canary Token Detector for Microsoft Word Document"
+  echo "Canary Token Detector for Microsoft Word Documents"
   echo
-  read -p "Please Enter Word Document File name : " file
+  read -p "Please enter a Word document file name: " file
 fi
 
 if [ ! -f $file ]; then
@@ -26,12 +26,13 @@ if [[ $file == *.docx ]] || [[ $file == *.doc ]]; then
   if grep -q canarytoken word/footer2.xml word/footer1.xml word/footer3.xml word/header1.xml word/header2.xml word/header3.xml; then
     tput setaf 1
     tput bel
-    echo "[x] canary token detected!"
+    echo "[x] Canary token detected!"
     tput sgr0
   else
     echo "This file appears normal"
   fi
   rm -rf word docProps _rels
 else
-  echo "This is not a word document file, select correct file"
+  echo "This is not a valid word document file" > 2
+  exit 1
 fi
